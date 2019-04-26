a3ac2d9ea5273c34d871d514d5d2b6a7be1a582c
Jan Poniatowski <poniatowski126@gmail.com>
2019-04-26 11:53:53 +0200

    return user and delete comments

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,13 @@
-const {validate} = require('../services/routing');
-const User = require('../models/User');
+const { validate } = require("../services/routing");
+const User = require("../models/User");
 
 const routes = {
-    validate: async (req, res) => {
-        const data = req.body;
-        //const password = req.password;
-        validate(res, User, data)
-    }
+  validate: async (req, res) => {
+    const data = req.body;
+    validate(res, User, data);
+  }
 };
 
 module.exports = {
-    routes,
+  routes
 };
--- services/routing.js
+++ services/routing.js
@@ -1,101 +1,98 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-exports.create = async (res, Model, data) =>{
-    const model = new Model(data);
-    try{
-        const result = await model.save();
-        res.status(200).send(result);
-    } catch (ex) {
-        console.error(ex);
-        res.sendStatus(500);
-    }
-}
+exports.create = async (res, Model, data) => {
+  const model = new Model(data);
+  try {
+    const result = await model.save();
+    res.status(200).send(result);
+  } catch (ex) {
+    console.error(ex);
+    res.sendStatus(500);
+  }
+};
 exports.getAll = async (res, Model) => {
-    try {
-        const model = await Model.find();
-        res.status(200).send(model);
-    } catch (ex) {
-        console.error(ex);
-        res.sendStatus(500);
-    }
-}
+  try {
+    const model = await Model.find();
+    res.status(200).send(model);
+  } catch (ex) {
+    console.error(ex);
+    res.sendStatus(500);
+  }
+};
 
-exports.getById = async (res,Model,id) => {
-    try{
-        const modelInstance= await Model.findById(id);
-        res.status(200).send(modelInstance);
-    } catch (ex){
-        console.log(ex);
-        res.sendStatus(500);
-    }
-}
+exports.getById = async (res, Model, id) => {
+  try {
+    const modelInstance = await Model.findById(id);
+    res.status(200).send(modelInstance);
+  } catch (ex) {
+    console.log(ex);
+    res.sendStatus(500);
+  }
+};
 
 exports.deleteById = async (res, Model, id) => {
-    try {
-        const modelInstance = await Model.findByIdAndDelete(id);
-        res.status(200).send(modelInstance);
-    } catch (ex) {
-        console.log(ex);
-        res.sendStatus(500);
-    }
-}
+  try {
+    const modelInstance = await Model.findByIdAndDelete(id);
+    res.status(200).send(modelInstance);
+  } catch (ex) {
+    console.log(ex);
+    res.sendStatus(500);
+  }
+};
 
-exports.updateModel= async(res,Model,data,id) => {
-    try{
-        const modelInstance = await Model.findById(id);
-        for (key of Object.keys(data))
-        {
-        modelInstance[key] = data[key];
-        }
-        const result = await modelInstance.save();
-        res.status(200).send(result);
-    } catch(ex){
-        console.log(ex);
-        res.sendStatus(500);
+exports.updateModel = async (res, Model, data, id) => {
+  try {
+    const modelInstance = await Model.findById(id);
+    for (key of Object.keys(data)) {
+      modelInstance[key] = data[key];
     }
-}
+    const result = await modelInstance.save();
+    res.status(200).send(result);
+  } catch (ex) {
+    console.log(ex);
+    res.sendStatus(500);
+  }
+};
 
-exports.deleteModel =async(res,Model) => {
-    try{
+exports.deleteModel = async (res, Model) => {
+  try {
     const modelInstance = await Model.remove();
-     res.status(200).send(modelInstance);
-    } catch (ex){
-        console.log(ex);
-        res.sendStatus(500);
-    }
-}
+    res.status(200).send(modelInstance);
+  } catch (ex) {
+    console.log(ex);
+    res.sendStatus(500);
+  }
+};
 
 ////////////
 exports.validate = async (res, Model, data) => {
-    try {
-        const user = await Model.findOne({'email' : data.email});
-        if (!user) return res.status(400).send('Invalid email or password.');
+  try {
+    const user = await Model.findOne({ email: data.email });
+    if (!user) return res.status(400).send("Invalid email or password.");
 
-        const validPassword = await bcrypt.compare(data.password, user.password);
-        //const validPassword = (data.password===user.password);
-        //console.log(data.password + ' ' + user.password);
-        if (!validPassword) return res.status(400).send('Invalid email or password.');
+    const validPassword = await bcrypt.compare(data.password, user.password);
+    if (!validPassword)
+      return res.status(400).send("Invalid email or password.");
 
-        //res.status(200).send(user);
-        res.status(200).send(true);
-    } catch (ex) {
-        console.log(ex);
-        res.sendStatus(500);
-    }
-}
+    res.status(200).send(user);
+  } catch (ex) {
+    console.log(ex);
+    res.sendStatus(500);
+  }
+};
 
 exports.createUser = async (res, Model, data) => {
-    const user = new Model();
-    user.email = data.email;
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(data.password, salt);
+  const user = new Model();
+  user.email = data.email;
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(data.password, salt);
 
-    try {
-        const result = await user.save();
-        res.status(200).send(result);
-    } catch (ex) {
-        console.error(ex);
-        res.sendStatus(500);
-    }
-}
+  try {
+    const result = await user.save();
+    res.status(200).send(result);
+  } catch (ex) {
+    console.error(ex);
+    res.sendStatus(500);
+  }
+};
